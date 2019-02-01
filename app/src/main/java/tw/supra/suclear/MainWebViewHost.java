52792c8b82f90051fafd060ed93a9b27359c0a49
wangjia20 <wangjia20@baidu.com>
2019-02-01 18:14:40 +0800

    更新gradle的配置，引入jetpack，迁移至androidx

--- app/src/main/java/tw/supra/suclear/MainWebViewHost.java
+++ app/src/main/java/tw/supra/suclear/MainWebViewHost.java
@@ -1,7 +1,7 @@
 package tw.supra.suclear;
 
 import android.graphics.Bitmap;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
