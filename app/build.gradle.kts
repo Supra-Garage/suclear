51eab3480016946a74d2c4ba48e5d83badecadb2
wangjia20 <wangjia20@baidu.com>
2019-02-01 19:54:09 +0800

    更新gradle的配置，dsl迁移至kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+
+
+
+val kotlin_version = "1.3.11"
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "tw.supra.suclear"
+        minSdkVersion(23)
+        targetSdkVersion(28)
+        versionCode = 2
+        versionName = "1.0.1-alpha"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            setMinifyEnabled(false)
+            proguardFiles( getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        setTargetCompatibility(1.8)
+        setSourceCompatibility(1.8)
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.arch.core:core-common:2.0.0")
+//    implementation("androidx.arch.core:core:2.0.0-rc01")
+    implementation("androidx.core:core:1.1.0-alpha04")
+    implementation("androidx.core:core-ktx:1.1.0-alpha04")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.annotation:annotation:1.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.1.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0")
+    implementation("android.arch.navigation:navigation-fragment-ktx:1.0.0-alpha11")
+    implementation("android.arch.navigation:navigation-ui-ktx:1.0.0-alpha11")
+    implementation("androidx.vectordrawable:vectordrawable:1.0.1")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.mediarouter:mediarouter:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.palette:palette:1.0.0")
+    implementation("androidx.leanback:leanback:1.0.0")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+    implementation("io.reactivex.rxjava2:rxandroid:2.0.2")
+    implementation("tw.supra.lib:supower:0.1.9")
+    implementation("com.google.code.gson:gson:2.8.2")
+    implementation("com.squareup.okhttp3:okhttp:3.12.1")
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.github.bumptech.glide:glide:4.0.0-RC1")
+    implementation("com.koushikdutta.ion:ion:2.2.1")
+    implementation("net.grandcentrix.tray:tray:0.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.0.0-RC1")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.2-alpha01")
+    implementation("com.mikepenz:materialdrawer:6.1.1")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+
+    val kotlin_version = "1.3.11"
+
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha11")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+//task clean (type: Delete) {
+//    delete rootProject . buildDir
+//}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
+
+//ext.deps = [:]
+//def versions =[:]
+//versions.lifecycle = "1.1.1"
+//versions.support = "28.1.1"
+//versions.kotlin = kotlin_version
+//versions.navigation = "1.0.0-alpha01"
+//versions.min_sdk = 23
+////versions.target_sdk = 28
+//versions.constraint_layout = "1.0.2"
+//ext.versions = versions
