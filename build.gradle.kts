91c5268dc437494416c74f17caeecc01751d7eb0
wangjia20 <wangjia20@baidu.com>
2019-11-29 17:56:18 +0800

    update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
-        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha11")
+        classpath("com.android.tools.build:gradle:3.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,10 +22,6 @@ allprojects {
         jcenter()
     }
 }
-
-//task clean (type: Delete) {
-//    delete rootProject . buildDir
-//}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
